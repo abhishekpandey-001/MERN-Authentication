5207f083b4221f6e1ee6405487994412f3de0d86
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2026-05-28 22:10:31 +0530

    fixed cors issues

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -50,7 +50,7 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 3 * 24 * 60 * 60 * 1000,
     });
 
@@ -112,7 +112,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 3 * 24 * 60 * 60 * 1000,
     });
 
